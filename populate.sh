96d9d019481a300246a5f948109c128d17656e20
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2020-09-11 16:56:56 +0530

    script: make modular

--- populate.sh
+++ populate.sh
@@ -6,12 +6,14 @@ cd workingdir || exit 1
 #*************** GET REQUIRED LIST OF COMMITS *******************
 
 # get commits in second and not in first
-if [ ! -f commit_list_in5.8_and_not5.7 ]; then
-    echo -e "\nAggregating commits"
-    git log v5.8 ^v5.7 --no-merges --format="%H" > commit_list_in5.8_and_not5.7
-else
-    echo -e "\nFILE_EXISTS: commit_list_in5.8_and_not5.7"
-fi
+collect_commits () {
+    if [ ! -f commit_list_in5.8_and_not5.7 ]; then
+        echo -e "\nAggregating commits"
+        git log v5.8 ^v5.7 --no-merges --format="%H" > commit_list_in5.8_and_not5.7
+    else
+        echo -e "\nFILE_EXISTS: commit_list_in5.8_and_not5.7"
+    fi
+}
 
 #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
@@ -22,78 +24,125 @@ fi
 # BELOW WE RUN SCRIPT checkpatch.pl ON ALL COMMITS
 # ***************************************************************
 
-cd .. || exit 1
+run_script () {
+    cd .. || exit 1
 
-if [ ! -f "workingdir/checkpatch_result" ]; then
-    echo -e "Running script on commits"
-    scripts/checkpatch.pl -g v5.7..v5.8 --terse --show-types | grep -o "[a-z0-9]*:[0-9]*: [A-Z_]*:[A-Z0-9_]*" > workingdir/checkpatch_result
-else
-    echo -e "\nFILE_EXISTS: workingdir/checkpatch_result"
-fi
+    if [ ! -f "workingdir/checkpatch_result" ]; then
+        echo -e "Running script on commits"
+        scripts/checkpatch.pl -g v5.7..v5.8 --terse --show-types | grep -o "[a-z0-9]*:[0-9]*: [A-Z_]*:[A-Z0-9_]*" > workingdir/checkpatch_result
+    else
+        echo -e "\nFILE_EXISTS: workingdir/checkpatch_result"
+    fi
+
+    cd workingdir || exit 1
+}
+
+run_script_sequential () {
+    if [ ! -d "reports" ]; then mkdir || exit 1; fi
+    cd .. || exit 1
+
+    echo -e "Running script sequentially on commits"
+    while read line; do
+        scripts/checkpatch.pl -g $line --show-types > workingdir/reports/$line
+    done < workingdir/commit_list_in5.8_and_not5.7
+
+    cd workingdir || exit 1
+}
+
+run_script_no_merges () {
+    cd .. || exit 1
+
+    T=$(cat workingdir/commit_list_in5.8_and_not5.7)
+    if [ ! -f "workingdir/checkpatch_result" ]; then
+        echo -e "Running script on commits no-merges"
+        scripts/checkpatch.pl -g $T --terse --show-types | grep -o "[a-z0-9]*:[0-9]*: [A-Z_]*:[A-Z0-9_]*" > workingdir/checkpatch_result
+    else
+        echo -e "\nFILE_EXISTS: workingdir/checkpatch_result"
+    fi
+
+    cd workingdir || exit 1
+}
 
 
 # ***************************************************************
-# BELOW WE COLLECT COUNT FROM REPORTS
+# BELOW WE CATEGORISE COMMITS FROM ERRORS
 # ***************************************************************
-cd workingdir || exit 1
+generate_report() {
+    if [ ! -d "attention" ]; then mkdir attention || exit 1; fi
+    if [ ! -d "type" ]; then mkdir type || exit 1; fi
+    if [ ! -f "checkpatch_result" ]; then run_script_no_merges; fi
 
-if [ ! -d "attention" ]; then mkdir attention || exit 1; fi
-if [ ! -d "type" ]; then mkdir type || exit 1; fi
+    rm attention/*
+    rm type/*
 
-rm attention/*
-rm type/*
+    echo -e "\nGenerating reports..."
+    while read line; do
+        # echo $line
+        commit=$(echo $line | awk -F'[: ]' '{print $1}')
+        attention=$(echo $line | awk -F'[: ]' '{print $4}')
+        type=$(echo $line | awk -F'[: ]' '{print $5}')
 
-echo -e "\nGenerating reports..."
-while read line; do
-    # echo $line
-    commit=$(echo $line | awk -F'[: ]' '{print $1}')
-    attention=$(echo $line | awk -F'[: ]' '{print $4}')
-    type=$(echo $line | awk -F'[: ]' '{print $5}')
+        if [ ! "$commit" = "$(touch attention/$attention && tail -n 1 attention/$attention)" ]; then
+            echo $commit >> "attention/$attention"
+        fi
 
-    if [ ! "$commit" = "$(touch attention/$attention && tail -n 1 attention/$attention)" ]; then
-        echo $commit >> "attention/$attention"
-    fi
+        if [ ! "$commit" = "$(touch type/$type && tail -n 1 type/$type)" ]; then
+            echo $commit >> "type/$type"
+        fi
 
-    if [ ! "$commit" = "$(touch type/$type && tail -n 1 type/$type)" ]; then
-        echo $commit >> "type/$type"
-    fi
+        if [ ! "$commit" = "$(touch attention/${attention}_type && tail -n 1 attention/${attention}_type)" ]; then
+            echo $type >> "attention/${attention}_type"
+        fi
+    done < checkpatch_result
 
-    if [ ! "$commit" = "$(touch attention/${attention}_type && tail -n 1 attention/${attention}_type)" ]; then
-        echo $type >> "attention/${attention}_type"
-    fi
-done < checkpatch_result
+    sort attention/WARNING_type > temp
+    uniq temp > attention/WARNING_type
 
-sort attention/WARNING_type > temp
-uniq temp > attention/WARNING_type
+    sort attention/ERROR_type  > temp
+    uniq temp > attention/ERROR_type
 
-sort attention/ERROR_type  > temp
-uniq temp > attention/ERROR_type
-
-sort attention/CHECK_type > temp
-uniq temp > attention/CHECK_type
-rm temp
+    sort attention/CHECK_type > temp
+    uniq temp > attention/CHECK_type
+    rm temp
+}
 
 # ***************************************************************
 # BELOW WE COLLECT STATS FROM REPORTS
 # ***************************************************************
-CHKPTCH=( "$(cat checkpatch_result)" )
-if [ ! -f "stats" ]; then
-    echo -e "\nCollecting statistics"
-    echo -e "Total ERRORS:\t$(echo "$CHKPTCH" | grep ERROR: -c)" >> stats
-    echo -e "Total WARNINGS:\t$(echo "$CHKPTCH" | grep WARNING: -c)" >> stats
-    echo -e "Total CHECKS:\t$(echo "$CHKPTCH" | grep CHECK: -c)" >> stats
-    echo "" >> stats
+collect_stats() {
+    if [ ! -f "checkpatch_result" ]; then run_script_no_merges; fi
+    CHKPTCH=( "$(cat checkpatch_result)" )
 
-    echo -e "Type of ERRORS:\t\t$(cat attention/ERROR_type | wc -l)" >> stats
-    echo -e "Type of WARNINGS:\t$(cat attention/WARNING_type | wc -l)" >> stats
-    echo -e "Type of CHECKS:\t\t$(cat attention/CHECK_type | wc -l)" >> stats
-    echo "" >> stats
+    if [ ! -f "stats" ]; then
+        echo -e "\nCollecting statistics"
 
-    printf '\t%-15s %-15s %-40s\n' "OCCURRENCES" "COMMITS" "TYPE" >> stats
-    VAL=$( for f in $(ls -1 "type/"); do \
-            printf '\t%-15s %-15s %-40s\n' "$(echo "$CHKPTCH" | grep $f -c)" "$(cat type/$f | wc -l)" "$f"; \
-        done )
-    echo "$VAL" | sort -n >> stats
-else
-    echo -e "\nOld statistics found"
-fi
+        echo "$CHKPTCH" | cut -f 2- -d ' ' | grep "ERROR" | sort | uniq -c | less | sort -nr >> stats
+        echo "" >> stats
+        echo "$CHKPTCH" | cut -f 2- -d ' ' | grep "WARNING" | sort | uniq -c | less | sort -nr >> stats
+        echo "" >> stats
+        echo "$CHKPTCH" | cut -f 2- -d ' ' | grep "CHECK" | sort | uniq -c | less | sort -nr >> stats
+
+        printf '\t%-15s %-15s %-40s\n' "OCCURRENCES" "COMMITS" "TYPE" >> stats
+        VAL=$( for f in $(ls -1 "type/"); do \
+                printf '\t%-15s %-15s %-40s\n' "$(echo "$CHKPTCH" | grep $f -c)" "$(cat type/$f | wc -l)" "$f"; \
+            done )
+        echo "$VAL" | sort -n >> stats
+        
+        echo -e "Total ERRORS:\t$(echo "$CHKPTCH" | grep ERROR: -c)" >> stats
+        echo -e "Total WARNINGS:\t$(echo "$CHKPTCH" | grep WARNING: -c)" >> stats
+        echo -e "Total CHECKS:\t$(echo "$CHKPTCH" | grep CHECK: -c)" >> stats
+        echo "" >> stats
+
+        echo -e "Type of ERRORS:\t\t$(cat attention/ERROR_type | wc -l)" >> stats
+        echo -e "Type of WARNINGS:\t$(cat attention/WARNING_type | wc -l)" >> stats
+        echo -e "Type of CHECKS:\t\t$(cat attention/CHECK_type | wc -l)" >> stats
+        echo "" >> stats
+    else
+        echo -e "\nOld statistics found"
+    fi
+}
+
+collect_commits
+run_script
+generate_report
+collect_stats
